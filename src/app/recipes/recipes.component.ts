99f15becf9482dba7430a9b4f18669fd71679da7
SahinSarkar <sarkarsahin697971@gmail.com>
2021-05-26 21:09:03 +0530

    Created blank components with logical layout

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html'
+})
+export class RecipesComponent {
+
+  constructor() {
+  }
+}
