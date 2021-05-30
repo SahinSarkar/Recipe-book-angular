dafadf2160f26df7f3f3019b7a2a479d1dac1676
SahinSarkar <sarkarsahin697971@gmail.com>
2021-05-30 21:20:56 +0530

    - Implemented showing of either recipes or shopping list component using ngIf - Transferred logic from recipe-list to recipe-item component and connected them by property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  featureInView = 'recipes';
+
+  showFeature(feature: string) {
+    this.featureInView = feature;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +7,15 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() showRecipeEvent: EventEmitter<string> = new EventEmitter<string>();
+
   constructor() {
   }
 
   ngOnInit(): void {
   }
 
+  showFeature(feature: string) {
+    this.showRecipeEvent.emit(feature);
+  }
 }
